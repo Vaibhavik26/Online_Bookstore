d5d24b45e58b5fa7defd505e8f3967c1d651e757
vaibhavi nadiger <128151317+Vaibhavik26@users.noreply.github.com>
2024-08-20 20:18:58 +0530

    Add files via upload

--- booklist.php
+++ booklist.php
@@ -1,19 +1,19 @@
-<?php session_start();
+<?php
+require('includes/config.php');
+ session_start();
 
 	
-	$link=mysql_connect("localhost","root","")or die("Can't Connect...");
 	
-	mysql_select_db("shop",$link) or die("Can't Connect to Database...");
 	
 	$cat=$_GET['subcatid'];
 	
 	$totalq="select count(*) \"total\" from book where b_subcat='$cat'";
 	
-	$totalres=mysql_query($totalq,$link) or die("Can't Execute Query...");
-	$totalrow=mysql_fetch_assoc($totalres);
+	$totalres=mysqli_query($conn,$totalq) or die("Can't Execute Query...");
+	$totalrow=mysqli_fetch_assoc($totalres);
 	
 	
-	$page_per_page=1;
+	$page_per_page=6;
 	
 	$page_total_rec=$totalrow['total'];
 	
@@ -70,49 +70,62 @@
 									<h1 class="title"><?php echo $_GET['subcatnm'];?></h1>
 									<div class="entry">
 										
-										<table border="3" width="100%" >
+										<table border="4" width="100%" >
+											<br><br><br><br><br>
 											<?php
 												
-												/*$k=($page_current_page-1)*$page_per_page;
+												$k=($page_current_page-1)*$page_per_page;
 											
 												$query="select *from book where b_subcat='$cat' LIMIT ".$k .",".$page_per_page;
 	
-												$res=mysql_query($query,$link) or die("Can't Execute Query...");*/
+												$res=mysqli_query($conn,$query) or die("Can't Execute Query...");
 	
-												//$count=0;
-												while($row=mysql_fetch_assoc($res))
+												$count=0;
+												while($row=mysqli_fetch_assoc($res))
 												{
-													//if($count==0)		{
+													if($count==0)
+													{
 														echo '<tr>';
-													//}													
+													}	
 													echo '<td valign="top" width="20%" align="center">
 														<a href="detail.php?id='.$row['b_id'].'&cat='.$_GET['subcatnm'].'">
 														<img src="'.$row['b_img'].'" width="80" height="100">
 														<br>'.$row['b_nm'].'</a>
 													</td>';
-													//$count++;							
+													$count++;							
 													
-													//if($count==4)		{
+													if($count==2)
+													{
 														echo '</tr>';
-														//$count=0;}
+														$count=0;
+													}
 												}
 											?>
 											
 										</table>
 										
+										
+										<br><br><br>
+										<center>
 										<?php
 											
-											/*if($page_total_page>$page_current_page)
+											if($page_total_page>$page_current_page)
 											{
 												echo '<a href="booklist.php?subcatid='.$_GET['subcatid'].'&subcatnm='.$_GET['subcatnm'].'&page='.($page_current_page+1).'">Next</a>';
+											}
+											
+											for($i=1;$i<=$page_total_page;$i++)
+											{
+												echo '&nbsp;&nbsp;<a href="booklist.php?subcatid='.$_GET['subcatid'].'&subcatnm='.$_GET['subcatnm'].'&page='.$i.'">'.$i.'</a>&nbsp;&nbsp;';
 											}
 											
 											if($page_current_page>1)
 											{	
 												echo '<a href="booklist.php?subcatid='.$_GET['subcatid'].'&subcatnm='.$_GET['subcatnm'].'&page='.($page_current_page-1).'">Previous</a>';
 											}
-											*/
+											
 										?>
+										</center>
 									</div>
 									
 								</div>
